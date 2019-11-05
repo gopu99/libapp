62d5f098aada507d04dfc8324428971391656051
Balagopal Kanattil <balukanattil@gmail.com>
2019-11-06 00:21:37 +0530

    Use Raw SQL queries instead of ORM

--- bookmanager.py
+++ bookmanager.py
@@ -11,6 +11,7 @@ app = Flask(__name__)
 project_dir = os.path.dirname(os.path.abspath(__file__))
 database_file = "sqlite:///{}".format(os.path.join(project_dir, "bookdatabase.db"))
 app.config["SQLALCHEMY_DATABASE_URI"] = database_file
+app.config["SQLALCHEMY_ECHO"] = True
 db = SQLAlchemy(app)
 
 
@@ -35,7 +36,7 @@ class Book(db.Model):
 @app.route("/", methods=["GET", "POST"])
 def home():
     # books = Book.query.all()
-    query = text("select * from book")
+    query = text("SELECT * FROM book")
     books = [Book(data) for data in db.engine.execute(query)]
     return render_template("home.html", books=books)
 
@@ -59,7 +60,10 @@ def edit():
 @app.route("/books", methods=["GET"])
 def get_book():
     title = request.args.get("title")
-    book = Book.query.filter_by(title=title).first()
+    # book = Book.query.filter_by(title=title).first()
+    query = text("SELECT * FROM book WHERE title = \"{}\" limit 1".format(title))
+    book_tuple = db.engine.execute(query).first()
+    book = Book(book_tuple)
     print(book)
     return render_template("book.html", book=book)
 
@@ -70,9 +74,16 @@ def update():
     oldtitle = request.form.get("oldtitle")
     newauthor = request.form.get("newauthor")
     book = Book.query.filter_by(title=oldtitle).first()
-    book.title = newtitle
-    book.author = newauthor
-    db.session.commit()
+    # book.title = newtitle
+    # book.author = newauthor
+    # db.session.commit()
+    query = text("""UPDATE book
+    SET title = \"{}\", author = \"{}\"
+    WHERE
+    book.title = \"{}\"
+    AND book.author = \"{}\"
+    """.format(newtitle, newauthor, book.title, book.author))
+    db.engine.execute(query)
     return redirect("/")
 
 
@@ -80,8 +91,13 @@ def update():
 def delete():
     title = request.form.get("title")
     book = Book.query.filter_by(title=title).first()
-    db.session.delete(book)
-    db.session.commit()
+    # db.session.delete(book)
+    # db.session.commit()
+    query = text("""
+    DELETE FROM book 
+    WHERE book.title = \"{}\" AND book.author = \"{}\"
+    """.format(book.title, book.author))
+    db.engine.execute(query)
     return redirect("/")
 
 
